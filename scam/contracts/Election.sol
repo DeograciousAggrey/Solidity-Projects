67dc9d82266625c782f0b1594456da1e58cbf3f1
DeograciousAggrey <DeograciousAggrey@gmail.com>
2020-11-30 18:03:05 +0300

    Changed the election contracts

--- scam/contracts/Election.sol
+++ scam/contracts/Election.sol
@@ -2,11 +2,31 @@ pragma solidity ^0.5.16;
 
 contract Election{
 
-    string public candidate;
+    //Model a candidate
+struct candidate{
+    uint id;
+    string name;
+    uint voteCount;
+}
 
-    constructor() public{
-        candidate = "candidate 1";
 
 
-    }
+// Store candidate count
+
+uint public candidateCount;
+
+constructor candidates(){
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+    addCandidate("Candidate 3");
+}
+
+mapping(uint=>candidate) public candidates1;
+uint public candidateCount;
+function addCandidate(string memory _name) private {
+candidateCount++;
+candidates1[candidateCount] = candidate(candidateCount,_name,0);
+}
+
+    
 }
